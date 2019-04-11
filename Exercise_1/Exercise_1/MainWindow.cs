067a6242a16ffd976279a381cebecf88422fa5a8
Yeong Kim <yeongjkim@yahoo.com>
2019-04-11 12:59:34 -0500

    Initial commit

---
+++ Exercise_1/Exercise_1/MainWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    int i = 0;
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    private void ButtonClickMe_Clicked(object sender, EventArgs args)
+    {
+        i++;
+        this.lblMainText.Text = "The button was clicked " + i + " times.";
+    }
+
+}
