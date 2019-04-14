2d8a641680b640f433e8725013728c2d932cc81f
Yeong Kim <yeongjkim@yahoo.com>
2019-04-13 21:29:47 -0500

    4/13/2019

---
+++ Exercise_3/Exercise_3/MainWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    Double eWeight;
+    Double mWeight;
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    private void ButtonConvert_Clicked(object sender, EventArgs args)
+    {
+        eWeight = Convert.ToDouble(this.eWeightField.Text);
+        mWeight = (eWeight / 9.81) * 3.711;
+        mWeightField.Text = mWeight.ToString();
+    }
+}
+
